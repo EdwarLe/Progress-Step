74187770d46813d6e1a5380fe134251867e59011
Edwar León <edwar.leon.m@gmail.com>
2023-05-20 17:30:54 -0500

    ejecución del cambio del color al dar click en avanzar

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const btnPrev = document.getElementById('btn__prev');
 const btnNext = document.getElementById('btn__next');
-const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const num3 = document.getElementById('num3');
+const num4 = document.getElementById('num4');
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
 
 var contar = 0;
 
 btnNext.onclick = function () {
-    contar++;
     
     if (contar == 0) {
-        num1.classList.remove('num');
-        num1.classList.add('numBlue')
+        num2.classList.remove('num');
+        num2.classList.add('numBlue');
+        line1.classList.remove('line');
+        line1.classList.add('lineBlue')
+    } else if (contar == 1) {
+        num3.classList.remove('num');
+        num3.classList.add('numBlue');
+        line2.classList.remove('line');
+        line2.classList.add('lineBlue')
+    } else if (contar = 2) {
+        num4.classList.remove('num');
+        num4.classList.add('numBlue');
+        line3.classList.remove('line');
+        line3.classList.add('lineBlue')
     }
+
+    contar++;
+    console.log(contar)
 }
 
 
