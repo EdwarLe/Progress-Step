47e5be9ca3bc5bdaef5f9d70b717ff09f4580c3f
Edwar León <edwar.leon.m@gmail.com>
2023-05-20 19:11:37 -0500

    progreso completo con efectos de transición

--- script.js
+++ script.js
@@ -7,29 +7,60 @@ const line1 = document.getElementById('line1');
 const line2 = document.getElementById('line2');
 const line3 = document.getElementById('line3');
 
-var contar = 0;
+var contar = 1;
 
 btnNext.onclick = function () {
     
-    if (contar == 0) {
+    if (contar == 1) {
+        btnPrev.classList.remove('btn__gray');
+        btnPrev.classList.add('btn');
         num2.classList.remove('num');
         num2.classList.add('numBlue');
         line1.classList.remove('line');
-        line1.classList.add('lineBlue')
-    } else if (contar == 1) {
+        line1.classList.add('lineBlue');
+        contar++;
+    } else if (contar == 2) {
         num3.classList.remove('num');
         num3.classList.add('numBlue');
         line2.classList.remove('line');
-        line2.classList.add('lineBlue')
-    } else if (contar = 2) {
+        line2.classList.add('lineBlue');
+        contar++;
+    } else if (contar == 3) {
         num4.classList.remove('num');
         num4.classList.add('numBlue');
         line3.classList.remove('line');
-        line3.classList.add('lineBlue')
-    }
+        line3.classList.add('lineBlue');
+        btnNext.classList.remove('btn');
+        btnNext.classList.add('btn__gray');
+        contar++;
+    } 
+}
 
-    contar++;
-    console.log(contar)
+btnPrev.onclick = function () {
+    
+    if (contar == 4) {
+        btnNext.classList.add('btn');
+        btnNext.classList.remove('btn__gray');
+        num4.classList.add('num');
+        num4.classList.remove('numBlue');
+        line3.classList.add('line');
+        line3.classList.remove('lineBlue');
+        contar--;
+    } else if (contar == 3) {
+        num3.classList.add('num');
+        num3.classList.remove('numBlue');
+        line2.classList.add('line');
+        line2.classList.remove('lineBlue');
+        contar--;
+    } else if (contar == 2) {
+        num2.classList.add('num');
+        num2.classList.remove('numBlue');
+        line1.classList.add('line');
+        line1.classList.remove('lineBlue');
+        btnPrev.classList.remove('btn');
+        btnPrev.classList.add('btn__gray');
+        contar--;
+    } 
 }
 
 
